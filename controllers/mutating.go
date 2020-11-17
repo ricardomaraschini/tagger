3544438780ec440955257845f946ed02df3d3444
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2020-11-17 19:36:03 +0100

    Adding controller for registry webhooks
    We now support webhooks coming in from quay.io and docker.io.

--- controllers/mutating.go
+++ controllers/mutating.go
@@ -20,8 +20,8 @@ import (
 	"github.com/ricardomaraschini/tagger/services"
 )
 
-// WebHook handles Mutation requests from kubernetes api.
-type WebHook struct {
+// MutatingWebHook handles Mutation requests from kubernetes api.
+type MutatingWebHook struct {
 	key     string
 	cert    string
 	bind    string
@@ -29,12 +29,12 @@ type WebHook struct {
 	decoder runtime.Decoder
 }
 
-// NewWebHook returns a web hook handler for kubernetes api mutation
+// NewMutatingWebHook returns a web hook handler for kubernetes api mutation
 // requests.
-func NewWebHook(tagsvc *services.Tag) *WebHook {
+func NewMutatingWebHook(tagsvc *services.Tag) *MutatingWebHook {
 	runtimeScheme := runtime.NewScheme()
 	codecs := serializer.NewCodecFactory(runtimeScheme)
-	return &WebHook{
+	return &MutatingWebHook{
 		key:     "assets/server.key",
 		cert:    "assets/server.crt",
 		bind:    ":8080",
@@ -44,14 +44,14 @@ func NewWebHook(tagsvc *services.Tag) *WebHook {
 }
 
 // Name returns a name identifier for this controller.
-func (wh *WebHook) Name() string {
-	return "webhook"
+func (m *MutatingWebHook) Name() string {
+	return "mutating webhook"
 }
 
 // responseError writes on the response an AdmissionReview with response status
 // set to an error. If AdmissionReview contains an UID that is inserted into
 // the reply.
-func (wh *WebHook) responseError(w http.ResponseWriter, req *admnv1.AdmissionReview, err error) {
+func (m *MutatingWebHook) responseError(w http.ResponseWriter, req *admnv1.AdmissionReview, err error) {
 	var ruid types.UID
 	if req.Request != nil {
 		ruid = req.Request.UID
@@ -80,7 +80,7 @@ func (wh *WebHook) responseError(w http.ResponseWriter, req *admnv1.AdmissionRev
 
 // responseAuthorized informs kubernetes the object creation is authorized
 // without modifications (patch to be applied).
-func (wh *WebHook) responseAuthorized(w http.ResponseWriter, req *admnv1.AdmissionReview) {
+func (m *MutatingWebHook) responseAuthorized(w http.ResponseWriter, req *admnv1.AdmissionReview) {
 	var ruid types.UID
 	if req.Request != nil {
 		ruid = req.Request.UID
@@ -106,38 +106,38 @@ func (wh *WebHook) responseAuthorized(w http.ResponseWriter, req *admnv1.Admissi
 }
 
 // tag validates a tag during update.
-func (wh *WebHook) tag(w http.ResponseWriter, r *http.Request) {
+func (m *MutatingWebHook) tag(w http.ResponseWriter, r *http.Request) {
 	reviewReq := &admnv1.AdmissionReview{}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		klog.Errorf("error reading body: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
-	if _, _, err := wh.decoder.Decode(body, nil, reviewReq); err != nil {
+	if _, _, err := m.decoder.Decode(body, nil, reviewReq); err != nil {
 		klog.Errorf("cant decoding body: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
 	objkind := reviewReq.Request.Kind.Kind
 	if objkind != "Tag" {
 		klog.Errorf("received event for %s, authorizing", objkind)
-		wh.responseAuthorized(w, reviewReq)
+		m.responseAuthorized(w, reviewReq)
 		return
 	}
 
 	var tag imgtagv1.Tag
 	if err := json.Unmarshal(reviewReq.Request.Object.Raw, &tag); err != nil {
 		klog.Errorf("unable to decode tag: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
 	tw := services.TagWrapper{Tag: &tag}
 	if err := tw.ValidateTagGeneration(); err != nil {
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
@@ -162,18 +162,18 @@ func (wh *WebHook) tag(w http.ResponseWriter, r *http.Request) {
 }
 
 // pod handles mutation requests made by kubernetes api with regards to pods.
-func (wh *WebHook) pod(w http.ResponseWriter, r *http.Request) {
+func (m *MutatingWebHook) pod(w http.ResponseWriter, r *http.Request) {
 	reviewReq := &admnv1.AdmissionReview{}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		klog.Errorf("error reading body: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
-	if _, _, err := wh.decoder.Decode(body, nil, reviewReq); err != nil {
+	if _, _, err := m.decoder.Decode(body, nil, reviewReq); err != nil {
 		klog.Errorf("cant decode body: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
@@ -182,24 +182,24 @@ func (wh *WebHook) pod(w http.ResponseWriter, r *http.Request) {
 	objkind := reviewReq.Request.Kind.Kind
 	if objkind != "Pod" {
 		klog.Errorf("strange event for a %s, authorizing", objkind)
-		wh.responseAuthorized(w, reviewReq)
+		m.responseAuthorized(w, reviewReq)
 		return
 	}
 
 	var pod corev1.Pod
 	if err := json.Unmarshal(reviewReq.Request.Object.Raw, &pod); err != nil {
 		klog.Errorf("error decoding raw object: %s", err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
 	// XXX namespaces come in empty, set it here.
 	pod.Namespace = reviewReq.Request.Namespace
 
-	patch, err := wh.tagsvc.PatchForPod(pod)
+	patch, err := m.tagsvc.PatchForPod(pod)
 	if err != nil {
 		klog.Errorf("error patching %s: %s", objkind, err)
-		wh.responseError(w, reviewReq, err)
+		m.responseError(w, reviewReq, err)
 		return
 	}
 
@@ -212,7 +212,7 @@ func (wh *WebHook) pod(w http.ResponseWriter, r *http.Request) {
 		patchData, err = json.Marshal(patch)
 		if err != nil {
 			klog.Errorf("error marshaling patch: %s", err)
-			wh.responseError(w, reviewReq, err)
+			m.responseError(w, reviewReq, err)
 			return
 		}
 	}
@@ -242,11 +242,11 @@ func (wh *WebHook) pod(w http.ResponseWriter, r *http.Request) {
 // Start puts the http server online. Requests for resources related to
 // deploys (Deployments and Pods) are set to deploy() handler while
 // image tag resources are managed by tag() handler.
-func (wh *WebHook) Start(ctx context.Context) error {
-	http.HandleFunc("/pod", wh.pod)
-	http.HandleFunc("/tag", wh.tag)
+func (m *MutatingWebHook) Start(ctx context.Context) error {
+	http.HandleFunc("/pod", m.pod)
+	http.HandleFunc("/tag", m.tag)
 	server := &http.Server{
-		Addr: wh.bind,
+		Addr: m.bind,
 	}
 
 	go func() {
@@ -258,7 +258,7 @@ func (wh *WebHook) Start(ctx context.Context) error {
 		}
 	}()
 
-	if err := server.ListenAndServeTLS(wh.cert, wh.key); err != nil {
+	if err := server.ListenAndServeTLS(m.cert, m.key); err != nil {
 		if err == http.ErrServerClosed {
 			return nil
 		}
