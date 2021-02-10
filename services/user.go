7c1fb662892ec9eaed2e82546a87bf89822afaa4
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-02-10 15:55:42 +0100

    Adding entity for user token validations

---
+++ services/user.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	authev1 "k8s.io/api/authentication/v1"
+	authov1 "k8s.io/api/authorization/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corecli "k8s.io/client-go/kubernetes"
+)
+
+// User entity gather operations related to Kubernetes users such
+// as token validations and authorization.
+type User struct {
+	corcli corecli.Interface
+}
+
+// NewUser returns an User handler capable of managing authentication and
+// authorization for Kubernetes users.
+func NewUser(corcli corecli.Interface) *User {
+	return &User{
+		corcli: corcli,
+	}
+}
+
+// CanAccessTags returns nil if provided token is able to access Tag entities
+// in provided namespace.
+func (u *User) CanAccessTags(ctx context.Context, ns, token string) error {
+	tkreview := &authev1.TokenReview{
+		Spec: authev1.TokenReviewSpec{
+			Token: token,
+		},
+	}
+
+	tr, err := u.corcli.AuthenticationV1().TokenReviews().Create(
+		ctx, tkreview, metav1.CreateOptions{},
+	)
+	if err != nil {
+		return err
+	}
+	if !tr.Status.Authenticated {
+		return fmt.Errorf("user not authenticated")
+	}
+
+	subreview := &authov1.SubjectAccessReview{
+		Spec: authov1.SubjectAccessReviewSpec{
+			User:   tr.Status.User.Username,
+			Groups: tr.Status.User.Groups,
+			ResourceAttributes: &authov1.ResourceAttributes{
+				Namespace: ns,
+				Resource:  "tags",
+				Verb:      "get",
+				Group:     "images.io",
+			},
+		},
+	}
+
+	autho, err := u.corcli.AuthorizationV1().SubjectAccessReviews().Create(
+		ctx, subreview, metav1.CreateOptions{},
+	)
+	if err != nil {
+		return err
+	}
+
+	if !autho.Status.Allowed || autho.Status.Denied {
+		return fmt.Errorf("unauthorized access")
+	}
+	return nil
+}
