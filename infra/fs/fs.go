267861bf559243e664ede4ef22931ea02b389e61
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-02-14 22:55:03 +0100

    Closing file descriptors

--- infra/fs/fs.go
+++ infra/fs/fs.go
@@ -98,12 +98,13 @@ func (f *FS) ArchiveDirectory(srcdir string, dst io.Writer) error {
 				return nil
 			}
 
-			data, err := os.Open(file)
+			fp, err := os.Open(file)
 			if err != nil {
 				return err
 			}
+			defer fp.Close()
 
-			_, err = io.Copy(tw, data)
+			_, err = io.Copy(tw, fp)
 			return err
 		},
 	)
@@ -146,11 +147,12 @@ func (f *FS) UnarchiveFile(src io.Reader, dst string) error {
 		if err != nil {
 			return fmt.Errorf("error creating file: %w", err)
 		}
+		defer out.Close()
+
 		if _, err := io.Copy(out, tarfp); err != nil {
-			out.Close()
 			return fmt.Errorf("error decompressing: %w", err)
 		}
-		out.Close()
+		return nil
 	}
 	return nil
 }
