8f7929df5fe0b47fecbaaccadd53c8bf70766138
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-03-01 21:46:58 +0100

    Moving functions out of importer.go

--- infra/imagestore/registry.go
+++ infra/imagestore/registry.go
@@ -9,30 +9,28 @@ import (
 	"github.com/containers/image/v5/signature"
 	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
-	"github.com/hashicorp/go-multierror"
 
 	"github.com/ricardomaraschini/tagger/infra/fs"
 )
 
-// ImageStore wraps cals for iteracting with our backend registry.
-type ImageStore struct {
+// Registry wraps cals for iteracting with our backend registry.
+type Registry struct {
 	fs      *fs.FS
 	regaddr string
 	polctx  *signature.PolicyContext
 	regctx  *types.SystemContext
 }
 
-// NewImageStore creates an entity capable of load objects to or save objects
+// NewRegistry creates an entity capable of load objects to or save objects
 // from from a backend registry. When calling Load we push an image to the
 // registry, when calling Save we pull the image from the registry and store
-// into a local tar file. XXX This entity should be reviewed, adding cache
-// or even change the way we store images locally on save may be needed.
-func NewImageStore(
+// into a local tar file.
+func NewRegistry(
 	regaddr string,
 	sysctx *types.SystemContext,
 	polctx *signature.PolicyContext,
-) *ImageStore {
-	return &ImageStore{
+) *Registry {
+	return &Registry{
 		fs:      fs.New(""),
 		regaddr: regaddr,
 		polctx:  polctx,
@@ -44,7 +42,7 @@ func NewImageStore(
 // Uses srcctx (of type types.SystemContext) when reading image from srcref,
 // so when copying from one remote registry into our cache registry srcctx
 // must contain all needed authentication information.
-func (i *ImageStore) Load(
+func (i *Registry) Load(
 	ctx context.Context,
 	srcref types.ImageReference,
 	srcctx *types.SystemContext,
@@ -81,7 +79,7 @@ func (i *ImageStore) Load(
 // file on disk.  Returns an ImageReference pointing to the local tar file
 // and a function the caller needs to call in order to clean up (property
 // close tar file and delete it from disk).
-func (i *ImageStore) Save(
+func (i *Registry) Save(
 	ctx context.Context, ref types.ImageReference,
 ) (types.ImageReference, func(), error) {
 	dstref, cleanup, err := i.NewLocalReference()
@@ -101,63 +99,9 @@ func (i *ImageStore) Save(
 	return dstref, cleanup, nil
 }
 
-// getImageTagHash attempts to fetch image hash remotely using provided system
-// context.  By image hash I mean the full image path with its hash, something
-// like: quay.io/tagger/tagger@sha256:... The ideia here is that the "from"
-// reference points to a image by tag (e.g. quay.io/tagger/taggger:latest).
-func (i *ImageStore) getImageTagHash(
-	ctx context.Context, from types.ImageReference, sysctx *types.SystemContext,
-) (types.ImageReference, error) {
-	img, err := from.NewImage(ctx, sysctx)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create image closer: %w", err)
-	}
-	defer img.Close()
-
-	manifestBlob, _, err := img.Manifest(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("unable to fetch image manifest: %w", err)
-	}
-
-	dgst, err := manifest.Digest(manifestBlob)
-	if err != nil {
-		return nil, fmt.Errorf("error calculating manifest digest: %w", err)
-	}
-
-	refstr := fmt.Sprintf("docker://%s@%s", from.DockerReference().Name(), dgst)
-	hashref, err := alltransports.ParseImageName(refstr)
-	if err != nil {
-		return nil, err
-	}
-	return hashref, nil
-}
-
-// GetImageTagHash attempts to obtain the hash for a given image on a remote registry.
-// It runs through provided system contexts trying all of them. If no SystemContext
-// is present it does one attemp without authentication. Returns the image reference
-// and the SystemContext that worked (the one whose credentials work) or an error.
-func (i *ImageStore) GetImageTagHash(
-	ctx context.Context, imgref types.ImageReference, sysctxs []*types.SystemContext,
-) (types.ImageReference, *types.SystemContext, error) {
-	// if no contexts then we do an attempt without using any credentials.
-	if len(sysctxs) == 0 {
-		sysctxs = []*types.SystemContext{nil}
-	}
-
-	var errors *multierror.Error
-	for _, sysctx := range sysctxs {
-		imghash, err := i.getImageTagHash(ctx, imgref, sysctx)
-		if err == nil {
-			return imghash, sysctx, nil
-		}
-		errors = multierror.Append(errors, err)
-	}
-	return nil, nil, fmt.Errorf("unable to get hash for image tag: %w", errors)
-}
-
 // NewLocalReference returns an image reference pointing to a local tar file. Also
 // returns a clean up function that must be called to free resources.
-func (i *ImageStore) NewLocalReference() (types.ImageReference, func(), error) {
+func (i *Registry) NewLocalReference() (types.ImageReference, func(), error) {
 	tfile, cleanup, err := i.fs.TempFile()
 	if err != nil {
 		return nil, nil, fmt.Errorf("error creating temp file: %w", err)
--- services/tagio.go
+++ services/tagio.go
@@ -18,11 +18,12 @@ import (
 )
 
 // TagIO is an entity that gather operations related to Tag images input and
-// output. This entity allow users to pull from or to push to a Tag.
+// output. This entity allow users to pull images from or to push images to
+// a Tag.
 type TagIO struct {
 	tagcli tagclient.Interface
 	taglis taglist.TagLister
-	impsvc *Importer
+	syssvc *SysContext
 }
 
 // NewTagIO returns a new TagIO object, capable of import and export Tag images.
@@ -39,43 +40,40 @@ func NewTagIO(
 	return &TagIO{
 		tagcli: tagcli,
 		taglis: taglis,
-		impsvc: NewImporter(corinf),
+		syssvc: NewSysContext(corinf),
 	}
 }
 
-// newTag returns a new tag object as if it is being pushed by the client.
-// From propert is set to stdin ("-") and the Tag is set as cacheable.
-func (t *TagIO) newTag(name string) *imagtagv1.Tag {
-	return &imagtagv1.Tag{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: imagtagv1.TagSpec{
-			From:  "-",
-			Cache: true,
-		},
-	}
-}
-
-// tagOrNew returns or an existing tag or a new one. If the tag exists
-// returns it otherwise creates a new one.
+// tagOrNew returns or an existing Tag or a new one.
 func (t *TagIO) tagOrNew(ns, name string) (*imagtagv1.Tag, error) {
 	it, err := t.taglis.Tags(ns).Get(name)
 	if err != nil && !errors.IsNotFound(err) {
 		return nil, err
 	}
-	if errors.IsNotFound(err) {
-		return t.newTag(name), nil
+
+	if err == nil {
+		return it, nil
 	}
-	return it, nil
+
+	return &imagtagv1.Tag{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: imagtagv1.TagSpec{
+			Cache: true,
+		},
+	}, nil
 }
 
-// Push reads a compressed tag from argument "from" and pushes it to our
-// registry storage. If success it then updates the tag accordingly to
-// indicate the new Generation.
-func (t *TagIO) Push(
-	ctx context.Context, ns, name string, fpath string,
-) error {
+// Push expects "fpath" to point to a valid docker image stored on disk as a tar
+// file, reads it and then pushes it to our cache registry through an image store
+// implementation (see infra/imagestore/registry.go).
+func (t *TagIO) Push(ctx context.Context, ns, name string, fpath string) error {
+	istore, err := t.syssvc.GetRegistryStore(ctx)
+	if err != nil {
+		return fmt.Errorf("error creating image store: %w", err)
+	}
+
 	it, err := t.tagOrNew(ns, name)
 	if err != nil {
 		return fmt.Errorf("error getting tag: %w", err)
@@ -87,32 +85,38 @@ func (t *TagIO) Push(
 		return fmt.Errorf("error parsing image name: %w", err)
 	}
 
-	pushedTo, err := t.impsvc.LoadTagImage(ctx, srcref, nil, ns, name)
+	// we pass nil as source context reference as to read the file from disk
+	// no authentication is needed. Namespace is used as repository and
+	// name as image name.
+	dstref, err := istore.Load(ctx, srcref, nil, ns, name)
 	if err != nil {
-		return fmt.Errorf("error loading image into cache registry: %w", err)
+		return fmt.Errorf("error loading image into registry: %w", err)
 	}
 
 	nextgen := it.NextGeneration()
-	it.Spec.Generation = nextgen
-	it.Status.Generation = nextgen
-
+	it.Spec.Generation, it.Status.Generation = nextgen, nextgen
 	it.RegisterImportSuccess()
 	it.PrependHashReference(
 		imagtagv1.HashReference{
 			Generation:     nextgen,
 			From:           "-",
 			ImportedAt:     metav1.Now(),
-			ImageReference: pushedTo,
+			ImageReference: dstref.DockerReference().String(),
 		},
 	)
 
-	// from this point on it is an Upsert logic.
-	if it.ObjectMeta.GetResourceVersion() == "" {
+	// that means we have received a push to a tag that does not
+	// exist yet, in such scenario there is no possible "from" to
+	// use on its spec so we set it to the place where we pushed
+	// the image.
+	if it.Spec.From == "" {
+		it.Spec.From = dstref.DockerReference().String()
 		_, err = t.tagcli.ImagesV1().Tags(ns).Create(
 			ctx, it, metav1.CreateOptions{},
 		)
 		return err
 	}
+
 	_, err = t.tagcli.ImagesV1().Tags(ns).Update(
 		ctx, it, metav1.UpdateOptions{},
 	)
@@ -122,18 +126,33 @@ func (t *TagIO) Push(
 // Pull saves a Tag image into a tar file and returns a reader from where the image
 // content can be read. Caller is responsible for cleaning up after the returned
 // resources by calling the returned function.
-func (t *TagIO) Pull(
-	ctx context.Context, ns string, name string,
-) (*os.File, func(), error) {
+func (t *TagIO) Pull(ctx context.Context, ns, name string) (*os.File, func(), error) {
 	it, err := t.taglis.Tags(ns).Get(name)
 	if err != nil {
 		return nil, nil, fmt.Errorf("error getting tag: %w", err)
 	}
 
-	fpath, cleanup, err := t.impsvc.SaveTagImage(ctx, it)
+	istore, err := t.syssvc.GetRegistryStore(ctx)
 	if err != nil {
-		return nil, nil, fmt.Errorf("error pulling tag to dir: %w", err)
+		return nil, nil, fmt.Errorf("error creating image store: %w", err)
 	}
+
+	imgref := it.CurrentReferenceForTag()
+	if len(imgref) == 0 {
+		return nil, nil, fmt.Errorf("reference for current generation not found")
+	}
+
+	from := fmt.Sprintf("docker://%s", imgref)
+	fromRef, err := alltransports.ParseImageName(from)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error parsing image reference: %w", err)
+	}
+
+	toRef, cleanup, err := istore.Save(ctx, fromRef)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error saving image locally: %w", err)
+	}
+	fpath := toRef.StringWithinTransport()
 
 	fp, err := os.Open(fpath)
 	if err != nil {
@@ -145,4 +164,32 @@ func (t *TagIO) Pull(
 		cleanup()
 	}
 	return fp, ncleanup, nil
+}
+
+// SaveTagImage pulls current generation of a given Tag into a local tar file.
+func (t *TagIO) SaveTagImage(
+	ctx context.Context, it *imagtagv1.Tag,
+) (string, func(), error) {
+	istore, err := t.syssvc.GetRegistryStore(ctx)
+	if err != nil {
+		return "", nil, fmt.Errorf("error creating image store: %w", err)
+	}
+
+	imgref := it.CurrentReferenceForTag()
+	if len(imgref) == 0 {
+		return "", nil, fmt.Errorf("reference for current generation not found")
+	}
+
+	from := fmt.Sprintf("docker://%s", imgref)
+	fromRef, err := alltransports.ParseImageName(from)
+	if err != nil {
+		return "", nil, fmt.Errorf("error parsing image reference: %w", err)
+	}
+
+	toRef, cleanup, err := istore.Save(ctx, fromRef)
+	if err != nil {
+		return "", nil, fmt.Errorf("error saving image locally: %w", err)
+	}
+
+	return toRef.StringWithinTransport(), cleanup, nil
 }
