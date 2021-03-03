d136773cbf34a33f023cdae3994780fdfdc29fcc
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-03-03 19:59:47 +0100

    Removing -n global flag
    Some of the commands don't need this flag. Added documentations for
    upgrade, downgrade and import.

--- cmd/kubectl-tag/downgrade.go
+++ cmd/kubectl-tag/downgrade.go
@@ -6,11 +6,19 @@ import (
 	"log"
 
 	"github.com/spf13/cobra"
+
+	"github.com/ricardomaraschini/tagger/cmd/kubectl-tag/static"
 )
 
+func init() {
+	tagdowngrade.Flags().StringP("namespace", "n", "", "Namespace to use")
+}
+
 var tagdowngrade = &cobra.Command{
-	Use:   "downgrade <image tag>",
-	Short: "Moves a tag to an older generation",
+	Use:     "downgrade <image tag>",
+	Short:   "Moves a tag to an older generation",
+	Long:    static.Text["downgrade_help_header"],
+	Example: static.Text["downgrade_help_examples"],
 	RunE: func(c *cobra.Command, args []string) error {
 		if len(args) != 1 {
 			return fmt.Errorf("provide an image tag")
--- cmd/kubectl-tag/upgrade.go
+++ cmd/kubectl-tag/upgrade.go
@@ -6,11 +6,19 @@ import (
 	"log"
 
 	"github.com/spf13/cobra"
+
+	"github.com/ricardomaraschini/tagger/cmd/kubectl-tag/static"
 )
 
+func init() {
+	tagupgrade.Flags().StringP("namespace", "n", "", "Namespace to use")
+}
+
 var tagupgrade = &cobra.Command{
-	Use:   "upgrade <image tag>",
-	Short: "Moves a tag to a newer generation",
+	Use:     "upgrade <image tag>",
+	Short:   "Moves a tag to a newer generation",
+	Long:    static.Text["upgrade_help_header"],
+	Example: static.Text["upgrade_help_examples"],
 	RunE: func(c *cobra.Command, args []string) error {
 		if len(args) != 1 {
 			return fmt.Errorf("provide an image tag")
