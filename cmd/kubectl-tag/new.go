24062be33b9857bbefca55f33d6c937fd41ec2d5
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-03-04 18:50:52 +0100

    Adding "kubect tag new" command

---
+++ cmd/kubectl-tag/new.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/ricardomaraschini/tagger/cmd/kubectl-tag/static"
+)
+
+func init() {
+	tagnew.Flags().StringP("namespace", "n", "", "Namespace to use")
+	tagnew.Flags().StringP("from", "f", "", "From where to import the tag")
+	tagnew.Flags().Bool("cache", false, "Cache the image into internal registry (mirror)")
+	tagnew.MarkFlagRequired("from")
+}
+
+var tagnew = &cobra.Command{
+	Use:     "new --from reg.io/repo/name:tag --cache -n namespace tagname",
+	Short:   "Imports a new generation for a tag",
+	Long:    static.Text["new_help_header"],
+	Example: static.Text["new_help_examples"],
+	RunE: func(c *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("provide an image tag name")
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		svc, err := CreateTagService()
+		if err != nil {
+			return err
+		}
+
+		ns, err := Namespace(c)
+		if err != nil {
+			return err
+		}
+
+		cache, err := c.Flags().GetBool("cache")
+		if err != nil {
+			return err
+		}
+
+		from, err := c.Flags().GetString("from")
+		if err != nil {
+			return err
+		}
+
+		return svc.NewTag(ctx, ns, args[0], from, cache)
+	},
+}
